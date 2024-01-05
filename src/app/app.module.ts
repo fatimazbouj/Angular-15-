b838190fa6b057cba5b8468468f96f5837c77a24
Fatima Zahra <fatima.zahra@alpha10x.com>
2024-01-05 16:47:57 +0100

    NgRx simple [first changes]

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,30 @@
 import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+//store
+import { StoreModule } from '@ngrx/store'
+import { StoreDevtoolsModule} from '@ngrx/store-devtools'
 
+
+import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { UsersModule } from './users/users.module';
+import { UsersListComponent } from './users/users-list/users-list.component';
+import { EffectsModule } from '@ngrx/effects';
+import { StoreRouterConnectingModule } from '@ngrx/router-store';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    StoreModule.forRoot({}),//ad reducer if needed
+    StoreDevtoolsModule.instrument(),
+    UsersModule,
+    EffectsModule.forRoot([]),
+    StoreRouterConnectingModule.forRoot(),
   ],
   providers: [],
   bootstrap: [AppComponent]
